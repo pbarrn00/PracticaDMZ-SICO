4c7ae73e3d7389562ef52b75816882858448d562
Pablo <pbarrn00@estudiantes.unileon.es>
2022-11-02 10:38:37 +0100

    T3 Done, connected int1 and ext1

--- fw/iptables.sh
+++ fw/iptables.sh
@@ -3,11 +3,11 @@
 # Activación del bit de forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Cambiar la pol ́ıtica por defecto de las cadenas INPUT y FORWARD para que descarten los paquetes.
+# Cambiar la política por defecto de las cadenas INPUT y FORWARD para que descarten los paquetes.
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 
-# Cambiar la pol ́ıtica de OUTPUT para que permita pasar todos los paquetes.
+# Cambiar la política de OUTPUT para que permita pasar todos los paquetes.
 iptables -P OUTPUT ACCEPT
 
 # Permitir el tráfico entrante a través de la interfaz de loopback
@@ -19,3 +19,11 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Permitir consultas entrantes de tipo ICMP ECHO REQUEST.
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
+# Comprobar que el tráfico Forward sale por las interfaces de red correctas.
+iptables -A FORWARD -p tcp -i eth2 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p udp -i eth2 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p icmp -i eth2 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A FORWARD -p tcp -i eth2 -o eth1 -s 10.5.2.0/24 -d 10.5.0.0/24 -m state --state NEW -j ACCEPT
+iptables -A FORWARD -p udp -i eth2 -o eth1 -s 10.5.2.0/24 -d 10.5.0.0/24 -m state --state NEW -j ACCEPT
+iptables -A FORWARD -p icmp -i eth2 -o eth1 -s 10.5.2.0/24 -d 10.5.0.0/24 -m state --state NEW -j ACCEPT
